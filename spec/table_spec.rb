b0ac53ffce0d5e9dea47532c1c1f3cb1106d3661
anthony.b.chung <anthony.b.chung@gmail.com>
2023-11-16 08:06:18 +1100

    merge table

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,38 +1,50 @@
+# frozen_string_literal: true
 
 require './table'
 require './robot'
 require './crane'
 
+# Table should just validate the position entered.
+# It does nothing more.
+
 describe Table do
-    it 'should be created' do
-    end
+  it 'should be created' do
+  end
 
-    it 'should initialize with width 5 and length 5' do
-        table = Table.new
+  it 'should initialize with width 5 and length 5' do
+    table = Table.new
 
-        width = table.width
-        length = table.length
+    width = table.width
+    length = table.length
 
-        expect(width).to be 5
-        expect(length).to be 5
-    end
+    expect(width).to be 5
+    expect(length).to be 5
+  end
 
-    it 'should return true if position is valid' do
-        table = Table.new 
-        robot = Robot.new
-        robot.x_pos = 2
-        robot.y_pos = 4
-        robot.face = "NORTH"
-        expect(table.position(robot)).to be true
-    end
+  it 'should return true if position is valid' do
+    Position = Struct.new(:x_pos, :y_pos)
+    position = Position.new(1, 2)
+    table = Table.new
+    valid_position = table.validate_position(position)
+    expect(valid_position).to be true
+  end
 
-    it 'should return false if the position is not valid' do
-        table = Table.new
-        robot = Robot.new
-        robot.x_pos = 3
-        robot.y_pos = 6
-        robot.face = "SOUTH"
-        expect(table.position(robot)).to be false
+  it 'should return false if the position is not valid' do
+    position = Position.new(6, 6)
+    table = Table.new
+    valid_position = table.validate_position(position)
+    expect(valid_position).to be false
+  end
 
-    end
+  it 'should return true if the position is 0,0 or 5,5' do
+    position = Position.new(5, 5)
+    table = Table.new
+    valid_position = table.validate_position(position)
+    expect(valid_position).to be true
+    position.x_pos = 0
+    position.y_pos = 0
+    valid_position = table.validate_position(position)
+    expect(valid_position).to be true
+  end
+  
 end
--- table.rb
+++ table.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-# Table should tell the system if the robot is on/off the table, or return the position of the robot.
+# Table should just validate the entered position.
+# It does nothing more.
 class Table
-    attr_accessor :width, :length
-    @@face_array = ["NORTH","EAST","SOUTH","WEST"]
+  attr_accessor :width, :length
 
-    def initialize
-        @width = 5
-        @length = 5
-    end
+  def initialize
+    @width = 5
+    @length = 5
+  end
 
-    def position(robot)
-        result = false
-        if (0..@width).include?(robot.x_pos) && (0..@length).include?(robot.y_pos) && @@face_array.include?(robot.face)
-            result = true
-        end
-        return result
+  def validate_position(position)
+    if position.x_pos.between?(0, @width) && position.y_pos.between?(0, @length)
+      true
+    else
+      false
     end
+  end
 end
